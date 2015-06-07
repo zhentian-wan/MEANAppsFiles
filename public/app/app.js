de4754855400851a56b8c42b8736c48f8f2bdf45
zhentian-wan <zhentian@monkeyexperience.com>
2015-06-08 00:49:59 +0300

    add animation

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,30 @@
 function AppController($rootScope, NOT_AUTHORIZED, $location) {
+
+    var vm = this;
+
     $rootScope.$on('$routeChangeError', function(event, current, previous, rejction) {
         if(rejction === NOT_AUTHORIZED) {
             $location.path('/');
         }
-    })
+    });
+
+    $rootScope.$on('$viewContentLoaded', function() {
+
+        d3.select('.jumbotron>h1')
+            .style('opacity', 0)
+            .transition()
+            .duration(900)
+            .style('opacity', 1);
+
+        d3.select('.jumbotron>blockquote')
+            .style('opacity', 0)
+            .style('margin', '0 200px 21px')
+            .transition()
+            .duration(900)
+            .delay(400)
+            .style('opacity', 1)
+            .style('margin', '0 0px 21px');
+    });
 }
 
 
